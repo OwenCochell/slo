993ada7e21e081c781d80205765a0529098f5de6
OwenCochell <owencochell@gmail.com>
2026-05-01 18:36:45 -0500

    Added more int types

--- arch/native/include/arch/stddef.hpp
+++ arch/native/include/arch/stddef.hpp
@@ -12,11 +12,19 @@
 #pragma once
 
 #include <cstddef>
+#include <cstdint>
 #include <cstdlib>
 
 namespace arch {
 
-/// Use C++ library size type
+/// We use the C++ standard library for all these types
+
 using size_t = std::size_t;
+using uint32_t = std::uint32_t;
+using int32_t = std::int32_t;
+using uint16_t = std::uint16_t;
+using int16_t = std::int16_t;
+using uint8_t = std::uint8_t;
+using int8_t = std::int8_t;
 
 } // namespace arch
